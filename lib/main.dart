636e7942f95c3442410a7e3e4f6c13d29e424b38
Abhishek Jain <abhishek.2022@tecorb.co>
2023-03-10 16:43:26 +0530

    update gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter_notifications/firebase_options.dart';
 
 //start firebase background services
 Future<void> backgroundHandler(RemoteMessage message) async {
